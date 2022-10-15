160f85569370c337ca0b516064b1160892d0cf40
Bartłomiej Ryczek <114954885+BartlomiejRyczek@users.noreply.github.com>
2022-10-15 18:19:56 +0200

    Delete pp1 directory

--- pp1/02-TypesAndVariables/zadanie17.py
+++
@@ -1,9 +0,0 @@
-print("Podaj swój wzrost w centymetrach: ")
-wzrost=float(input())
-x=(wzrost*0.032808399) #wzrost w stopach
-y=(wzrost*0.393700787) #wzrost w calach
-
-stopy=(round (x,3))
-cale=(round(y,2))
-
-print(f"Twój wzrost w stopach wynosi {stopy}, a w calach {cale}")
--- pp1/02-TypesAndVariables/zadanie22.py
+++
@@ -1,9 +0,0 @@
-x=input("Podaj zapłaconą kwotę: ")
-kwota=float(x)
-
-vat=kwota*0.23
-
-vatzaokraglony=round(vat, 2)
-
-print(f"Zapłacona kwota: {kwota}")
-print(f"VAT 23%: {vatzaokraglony}")
