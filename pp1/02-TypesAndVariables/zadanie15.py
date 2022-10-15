fb9b23e56e29f38e3ec01b1f6aefc7cdc3d05fea
Bartłomiej Ryczek <114954885+BartlomiejRyczek@users.noreply.github.com>
2022-10-15 18:17:53 +0200

    Add files via upload

---
+++ pp1/02-TypesAndVariables/zadanie15.py
@@ -0,0 +1,11 @@
+import math
+print(15*38) #przykład a
+y=3+4 #przykład b
+x=5+9
+print(x*y) 
+print(7//2) #przykład c
+print(48%5) #przykład d
+print((8+7+4+2)/4) #przykład e
+print(2**10) #przykład f
+print(math.sqrt(49)) #przykład g
+print(80*0.25)              #przykład h
