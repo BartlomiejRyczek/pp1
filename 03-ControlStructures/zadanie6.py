b299ee109e28a626e7a4c06e10e0d6440d6b88f5
Bartłomiej Ryczek <114954885+BartlomiejRyczek@users.noreply.github.com>
2022-10-17 11:18:50 +0200

    Add files via upload

---
+++ 03-ControlStructures/zadanie6.py
@@ -0,0 +1,8 @@
+x=input("Podaj prędkość w km/h: ")
+x=float(x)
+if x>130:
+    print("Pojazd przekroczył prędkość")
+else:
+    if x<130:
+        print("Samochód jedzie z odpowiednią prędkościa")
+
---
+++ 03-ControlStructures/zadanie7.py
@@ -0,0 +1,7 @@
+x=input("Podaj liczbę: ")
+x=float(x)
+if x%2 == 0:
+    print(f"{x} jest parzysta")
+else:
+    print(f"{x} jest nieparzysta")
+
