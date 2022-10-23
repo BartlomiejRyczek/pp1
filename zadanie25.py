4c833f9f11d588280e6e258a0cdd804d8a95803d
Bartłomiej Ryczek <114954885+BartlomiejRyczek@users.noreply.github.com>
2022-10-23 19:18:29 +0200

    Delete zadanie25.py

--- zadanie25.py
+++
@@ -1,7 +0,0 @@
-for i in range(4):
-    for j in range(16):
-        if (j==0 or j==15) or (i==0 or i==3) and (j>0 and j<15):
-            print("*", end="")
-        else:
-            print(end=" ")
-    print()
