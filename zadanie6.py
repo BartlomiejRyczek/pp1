bc3a771f1fb28e2d29dc18e10bb0fcaedd210830
Bartłomiej Ryczek <114954885+BartlomiejRyczek@users.noreply.github.com>
2022-10-23 19:19:23 +0200

    Delete zadanie6.py

--- zadanie6.py
+++
@@ -1,8 +0,0 @@
-x=input("Podaj prędkość w km/h: ")
-x=float(x)
-if x>130:
-    print("Pojazd przekroczył prędkość")
-else:
-    if x<130:
-        print("Samochód jedzie z odpowiednią prędkościa")
-
