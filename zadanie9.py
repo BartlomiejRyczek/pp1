eb2d5e8667797f6717e556995947a59f1a220027
Bartłomiej Ryczek <114954885+BartlomiejRyczek@users.noreply.github.com>
2022-10-23 19:19:41 +0200

    Delete zadanie9.py

--- zadanie9.py
+++
@@ -1,8 +0,0 @@
-x=input("Podaj pierwszą liczbę: ")
-x=int()
-y=input("Podaj drugą liczbe: ")
-y=int()
-if x>0: 
-    print("Pierwsza liczba jest dodatnia")
-if y>0:
-    print("Druga liczba jest dodatnia")
