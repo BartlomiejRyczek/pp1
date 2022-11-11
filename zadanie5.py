775985b62c21351c91aa0f1c0f767eab73ef905a
Bartłomiej Ryczek <114954885+BartlomiejRyczek@users.noreply.github.com>
2022-11-11 13:15:30 +0100

    Delete zadanie5.py

--- zadanie5.py
+++
@@ -1,9 +0,0 @@
-def display_university_address():
-    print("Cracow University of Economics")
-    print("Rakowicka 27")
-    print("31-510 Kraków, POLAND")
-
-for i in range(2):
-    print(display_university_address())
-
-
